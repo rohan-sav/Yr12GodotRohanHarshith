7b8f2776d31ea728daef9bc0e630279e1a97f393
rohan-sav <rohsavofficial@gmail.com>
2025-07-30 13:04:04 +1200

    Fixed Project

---
+++ testing/MainMenu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Collision.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://options.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
